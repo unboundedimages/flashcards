ee2da9a5bb45b889c22053fbf1568371f212ab2a
unboundedimages <majid.muhassan@gmail.com>
2018-07-10 10:04:15 -0400

    1)created 3 arrays in app.js. 2) created a previous and next function to advance the cards forwards and backwards.  3) removed mockup divs in the html

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-function myFunction() {
-	var x = document.getElementById("myDIV");
-	var div = "<div>&nbsp;&nbsp;&nbsp;9</div>"
-	if (x.innerHTML ==="" ) {
-		x.innerHTML =  div +
-		"<div>x 1</div>" +
-		"<div>___</div>" +
-		"<div>&nbsp;&nbsp;&nbsp;9</div>";
-	} else if(x.innerHTML ===  div +
-	"<div>x 1</div>" +
-	"<div>___</div>" +
-	"<div>&nbsp;&nbsp;&nbsp;9</div>") {
-		x.innerHTML =  "<div id='1'><ol>" +
-		"<li>*** *** ***</li>" +
-		"</ol></div>";
-	} 
-	
-	else {
-		x.innerHTML = "<div>&nbsp;&nbsp;&nbsp;9</div>"+
-		"<div>x 3</div>" +
-		"<div>___</div>" +
-		"<div>&nbsp;27</div>";
-	}
-}
+//make an array wiith numbers to be multiplied
+var multiplicand = [0,1,2,3,4,5,6,7,8,9,10,11,12]; 
+var product = [0,9,18,27,36,45,54,63,72,81,90,99,108]
+var stars = [0,"*","**","***","****","*****","******","*******","********","*********","**********","***********","************"]
+var Current = 0;
+
+//take the product of those numbers and inject them into product div
+//when next button is clicked, the next multiplicand and stars are put into the div
+document.getElementById("multiplicand").innerHTML = multiplicand[Current];
+
+function Prev(){
+	if(Current == 0){
+		Current = multiplicand.length && product.length - 1 && stars.length -1;}
+		else{
+			Current--;}
+
+			document.getElementById("multiplicand").innerHTML = multiplicand[Current];
+			document.getElementById("product").innerHTML = product[Current];
+			document.getElementById("stars").innerHTML = stars[Current];
+		}
+
+		function Next(){
+			if(Current == multiplicand.length -1 && product.length - 1 && stars.length -1){
+				Current = 0}
+				else{
+					Current++;}
+
+					document.getElementById("multiplicand").innerHTML = multiplicand[Current];
+					document.getElementById("product").innerHTML = product[Current];
+					document.getElementById("stars").innerHTML = stars[Current];
+				}
+
+
