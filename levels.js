3f4a882334416300e17995ee9771d83e393d7bdf
rodrossdev <majid.muhassan@gmail.com>
2019-03-08 19:51:39 -0500

    enclosed code inside event listener

--- levels.js
+++ levels.js
@@ -1,27 +1,32 @@
-let multiplier = 8;
-function range(start, end) {
-  return Array(end - start + 1)
-    .fill()
-    .map((_, idx) => start + idx);
-}
-var result = range(1, 15); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-var combined = multiplier * result;
-let uu;
-let text = [];
-for (uu = 0; uu < result.length; uu++) {
-  text.push(result[uu] * multiplier);
-}
-
 document.getElementById("user-selects").onchange = function() {
+  let multiplier = 8;
   let userSelect = document.getElementById("user-selects").value;
+
+  function range(start, end) {
+    return Array(end - start + 1)
+      .fill()
+      .map((_, idx) => start + idx);
+  }
+
+  var result = range(1, 15); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+  var combined = multiplier * result;
+  let text = [];
+  let uu;
+
+  for (uu = 0; uu < result.length; uu++) {
+    text.push(result[uu] * userSelect);
+  }
+
+  // document.getElementById("user-selects").onchange = function() {
+  //   let userSelect = document.getElementById("user-selects").value;
+  let userNum = Number(userSelect);
+  console.log("THIS IS USER SELECT: ", userNum);
+
+  document.getElementById("multiplicand").innerText = result;
   document.getElementById("multiplier").innerText = userSelect;
 
-  let product = Number(userSelect) * 8;
-  console.log(product);
+  document.getElementById("product").innerText = text;
+
+  console.log(result);
+  console.log(text);
 };
-
-document.getElementById("multiplicand").innerText = text[8];
-document.getElementById("product").innerText = result[2];
-
-console.log(result);
-console.log(text);
