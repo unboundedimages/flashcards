9f642b8f3e77551b2b19a67100c6768302dc8cb3
rodrossdev <majid.muhassan@gmail.com>
2019-03-19 18:32:28 -0400

    fixed the size of font on back of card for previous and next.  Fixed 0's and 10's array for next and previous that caused undefined error.

--- levels.js
+++ levels.js
@@ -12,7 +12,7 @@ document.getElementById("user-selects").onchange = function() {
   getRange();
   let starzArray = [];
   let joinStarz = [];
-  let starString = "";
+  let starString;
   var i;
   var s;
   function starz() {
@@ -27,37 +27,33 @@ document.getElementById("user-selects").onchange = function() {
     for (var iii = 0; iii < starzArray.length; iii++) {
       console.log(starzArray[iii]);
     }
-    // joinStarz.push(starzArray.join(" - "));
     joinStarz.push(starzArray.join("  "));
     starString = joinStarz[0].toString();
-    fin = starString.split("0");
-    fin.shift();
-    fin.shift();
-    fin.unshift("0");
+    fin = starString.split(" 0");
+    console.log("fin :", fin);
+    if (fin[0] === "0 ") {
+      document.getElementById("product").style.fontSize = "5.3em";
+      document.getElementById("product").style.top = "31%";
+    }
     console.log("this userNum inside of starz: ", userNum);
     if (userSelect.value == 0) {
       fin = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
   }
   starz();
-  // document.getElementById("stars").innerHTML = userNum;
   document.getElementById("stars").innerHTML = fin[0];
   document.getElementById("product-front").innerHTML = fin[0];
-  // if (productArray[2] == 0) {
-  //   document.getElementById("product-front").innerHTML = "wtf";
-  //   alert("this works>>> or nah????");
-  // }
-  console.log("grand finale ", fin);
+  console.log("grand finale ", fin[0]);
   console.log("this is the fn userNum:  ", userNum);
 };
 
+//create the range of numbers that will be multiplied - "0-15"
 function getRange() {
   function range(start, end) {
     return Array(end - start + 1)
       .fill()
       .map((_, idx) => start + idx);
   }
-  // var multiplicand = range(1, 15); // [9, 10, 11, 12, ....]
   var multiplicand = range(0, 15); // [9, 10, 11, 12, ....]
   (productArray = []), (Current = 0);
   let uu;
@@ -72,6 +68,8 @@ function getRange() {
     console.log("THIS IS THE multiplicand", multiplicand);
     console.log("THIS IS PRODUCTARRAY: ", productArray);
   }
+
+  //move to the next number
   function Next() {
     document.getElementById("next").onclick = function() {
       if (
@@ -88,9 +86,8 @@ function getRange() {
       document.getElementById("product-front").innerHTML =
         productArray[Current];
       document.getElementById("stars").innerHTML = fin[Current];
-      // psuedo
+      // size of number on the back of card
       if (fin[Current] <= 9) {
-        // alert("cool");
         document.getElementById("product").style.fontSize = "2.3em";
       }
 
@@ -99,14 +96,19 @@ function getRange() {
         document.getElementById("product").style.fontSize = "5.3em";
         document.getElementById("product").style.top = "31%";
       } else {
-        document.getElementById("product").style.fontSize = "2.3em";
+        document.getElementById("product").style.fontSize = "1.8em";
+        document.getElementById("product").style.top = "29%";
+      }
+      if (userSelect.value >= 10) {
+        // alert("we're good");
+        document.getElementById("product").style.fontSize = "1.8em";
         document.getElementById("product").style.top = "20%";
       }
       flipSound();
     };
   }
   Next();
-
+  //go back to previous number
   function Previous() {
     document.getElementById("prev").onclick = function() {
       if (Current == 0) {
@@ -120,14 +122,29 @@ function getRange() {
       document.getElementById("product-front").innerHTML =
         productArray[Current];
       document.getElementById("stars").innerHTML = fin[Current];
+      //sizes for the back of the card
+      if (fin[Current] <= 9) {
+        document.getElementById("product").style.fontSize = "2.3em";
+      }
 
-      if (fin[0] === 0) {
-        console.log("fin inside previous");
+      if (productArray[2] == 0) {
+        document.getElementById("product").innerHTML = productArray[Current];
+        document.getElementById("product").style.fontSize = "5.3em";
+        document.getElementById("product").style.top = "31%";
+      } else {
+        document.getElementById("product").style.fontSize = "1.8em";
+        document.getElementById("product").style.top = "29%";
+      }
+
+      if (userSelect.value >= 10) {
+        document.getElementById("product").style.fontSize = "1.8em";
+        document.getElementById("product").style.top = "20%";
       }
       flipSound();
     };
   }
 
+  //if user wishes to multiply by zero
   if (productArray[0] === productArray[1]) {
     console.log("productArray.length: ", productArray[0], productArray[1]);
     fin = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -138,7 +155,9 @@ function getRange() {
     document.getElementById("product").style.top = "20%";
   }
   Previous();
-  if (userSelect.value >= 1) {
+
+  //hide/show drop down select
+  if (userSelect.value) {
     document.getElementById("number-range").style.display = "none";
     playSound = false;
   }
